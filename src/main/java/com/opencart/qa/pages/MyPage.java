c44de36a54b885f1185806e383115efd28ddf196
Naveen AutomationLabs <naveenanimation20@gmail.com>
2026-04-02 07:25:21 +0530

    Remove unused login method and browser variable
    Removed redundant login method and browser variable.

--- src/main/java/com/opencart/qa/pages/MyPage.java
+++ src/main/java/com/opencart/qa/pages/MyPage.java
@@ -3,16 +3,6 @@ package com.opencart.qa.pages;
 public class MyPage {
 	
 	
-	String browser = "chrome";
-	
-	public void login() {
-		System.out.println("hello app");
-		System.out.println("hello app");
-		System.out.println("hello app");
-		System.out.println("hello app");
-	}
-	
-	
 	public void logout() {
 		System.out.println("hello logout");
 		
